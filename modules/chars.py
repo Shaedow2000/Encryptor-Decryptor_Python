e21dc364d7927223c72c78098ced67f28a2165ba
Shaedow2000 <amsal8795@gmail.com>
2025-12-09 18:03:07 +0100

    Create the encryptor program function.

--- modules/chars.py
+++ modules/chars.py
@@ -9,10 +9,11 @@ def shuffle( key: str ) -> list:
     # this function will be shuffling the chars list in a way that the shuffle stays the same if the user enters the same key.
     # in another way, if u enter a key then excrypt a message, you will not be able to decrypt it.
     # so to decrypt the message you need the same key used to encrypt the message.
-    chars: list[ str ] = list( string.printable + string.whitespace )
+    chars: list[ str ] = list( string.ascii_letters + string.digits + string.hexdigits + string.punctuation + ' ' )
+    shuffled_chars: list[ str ] = chars[ : ]
 
     lock = random.Random( key )
 
-    lock.shuffle( chars )
+    lock.shuffle( shuffled_chars )
 
-    return chars
+    return [ chars, shuffled_chars ]
--- modules/encryptor.py
+++ modules/encryptor.py
@@ -1,9 +1,22 @@
-import random
-import string
+from chars import shuffle
 
-def encrypt( msg: str, key: str ):
+def encrypt( msg: str, key: str ) -> str:
     """
     Encrypts messages with a specific key entered by the user.
     """
-    pass
+    
+    chars: list[ str ] = shuffle( key )[ 0 ]
+    shuffled_chars: list[ str ] = shuffle( key )[ 1 ]
+
+    list_msg: list[ str ] = list( msg )
+   
+    enc_msg_list: list[ str ] = []
+
+    for i in range( list_msg.__len__() ):
+        char_index: int = chars.index( list_msg[ i ] )
+        enc_msg_list.append( shuffled_chars[ char_index ] )
+
+    enc_msg: str = ''.join( enc_msg_list )
+
+    return enc_msg
 
