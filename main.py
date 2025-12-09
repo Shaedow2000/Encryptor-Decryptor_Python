f6ae3ccb0cbeb0462681ed2c20e2a949b35f10bb
Monium <wizardpearisherelol@gmail.com>
2025-12-09 17:11:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import random
 import time
 import string
 
-ourchar = " " + string.ascii_letters + string.digits + string.punctuation 
-char = list(ourchar)
+ourchar = " " + string.ascii_letters + string.digits + string.punctuation  #abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+char = list(ourchar) #Converts ourchar into a list so that we can shuffle it
 
-random.shuffle(char)
-
-print(char)
-
-#Encryption code 
+random.shuffle(char) #Shuffles the list char so that the characters are in a random order
 
 
+
+
+
+
+#User choice to continue or exit
 urchoice = input("Welcome to the decryption/encryption program! Press Q to exit and C to continue: ")
 
 while True:
@@ -26,29 +27,31 @@ while True:
     else:
         urchoice = input("Invalid input. Please press Q to exit and C to continue: ")
 
-
-encryption = input("Enter a message to encrypt : ")
-encrypted_text = " "
+#Encryption code
+encryption = input("Enter a message to encrypt : ") #Takes user input for encryption
+encrypted_text = " " #Empty string to store the encrypted text
 
 
 for i in encryption: #Baisically sees how many words there are in the encrypt variable and coverts them one by one
-    index = ourchar.index(i)
-    encrypted_text += char[index]
+    index = ourchar.index(i) #Finds the index of each character in the ourchar string
+    encrypted_text += char[index] #Adds the corresponding character from the shuffled char list to the encrypted_text string
 
-print(f"Original message : {encryption}")
-print(f"Encrypted message : {encrypted_text}")
+print(f"Original message : {encryption}") #Prints the original message
+print(f"Encrypted message : {encrypted_text}") #Prints the encrypted message
+
+
 #Decryption code
 
 
-decryption = input("Enter a message to decrypt : ")
-Dekrypted_text = ""
+decryption = input("Enter a message to decrypt : ") #Takes user input for decryption
+Dekrypted_text = "" #Empty string to store the decrypted text
 
-for i in decryption:
-    index = char.index(i)
-    Dekrypted_text += ourchar[index]
+for i in decryption: #Baisically sees how many words there are in the decrypt variable and coverts them one by one
+    index = char.index(i) #Finds the index of each character in the char list
+    Dekrypted_text += ourchar[index] #Adds the corresponding character from the ourchar string to the Dekrypted_text string
 
-print(f"Encrypted message : {decryption}")
-print(f"Decrypted message : {Dekrypted_text}")
+print(f"Encrypted message : {decryption}") #Prints the encrypted message
+print(f"Decrypted message : {Dekrypted_text}") #Prints the decrypted message
 
 
 
