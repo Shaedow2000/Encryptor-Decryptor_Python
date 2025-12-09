8ef0180d656ee404bd698531cc052566ecf23cdd
Shaedow2000 <amsal8795@gmail.com>
2025-12-09 18:31:35 +0100

    Add KeyboardInterrupt handler for the main function.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from modules.encryptor import encrypt
 from modules.decryptor import decrypt
@@ -9,39 +10,44 @@ def main() -> None:
     """
 
     print( '1: encrypt | 2: decrypt | c: clear screen | q: quit' )
-    while True:
-        inp: str = input( '==> ' ).replace( ' ', '' ).lower()
+    
+    try:
+        while True:
+            inp: str = input( '==> ' ).replace( ' ', '' ).lower()
+    
+            if inp == '':
+                continue
+            elif inp == 'c':
+                os.system( 'cls' if os.name == 'nt' else 'clear' )
+                print( '1: encrypt | 2: decrypt | c: clear screen | q: quit' )
+                continue
+            elif inp == 'q':
+                print( '--> Exiting...' )
+                break
+            elif inp == '1':
+                msg: str = input( '==> Message to encrypt: ' )
+                print( '>> Please enter a secure key and save it to decrypt messages that use this key...' )
+                key: str = input( '==> Key: ' )
 
-        if inp == '':
-            continue
-        elif inp == 'c':
-            os.system( 'cls' if os.name == 'nt' else 'clear' )
-            print( '1: encrypt | 2: decrypt | c: clear screen | q: quit' )
-            continue
-        elif inp == 'q':
-            print( '--> Exiting...' )
-            break
-        elif inp == '1':
-            msg: str = input( '==> Message to encrypt: ' )
-            print( '>> Please enter a secure key and save it to decrypt messages that use this key...' )
-            key: str = input( '==> Key: ' )
+                enc_msg: str = encrypt( msg, key )
 
-            enc_msg: str = encrypt( msg, key )
+                print( '\n!> Encrypted message:' )
+                print( enc_msg )
+            elif inp == '2':
+                enc_msg: str = input( '==> Message to decrypt: ' )
+                print( '>> Please enter the key used to encrypt this message...' )
+                key: str = input( '==> Key: ' )
 
-            print( '\n!> Encrypted message:' )
-            print( enc_msg )
-        elif inp == '2':
-            enc_msg: str = input( '==> Message to decrypt: ' )
-            print( '>> Please enter the key used to encrypt this message...' )
-            key: str = input( '==> Key: ' )
+                msg: str = decrypt( enc_msg, key )
 
-            msg: str = decrypt( enc_msg, key )
-
-            print( '\n!> Decrypted message:' )
-            print( msg )
-        else: 
-            print( f'!-> { inp } is an unknown command.' )
-            continue
+                print( '\n!> Decrypted message:' )
+                print( msg )
+            else: 
+                print( f'!-> [ { inp } ] is an unknown command.' )
+                continue
+    except KeyboardInterrupt:
+        print( '\n--> Exiting...' )
+        sys.exit( 1 )
 
 
 if __name__ == '__main__':
